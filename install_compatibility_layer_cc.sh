e538f5dc05b9e205424cb46bde616a3943ffdcca
Bart Oldeman <bart.oldeman@calculquebec.ca>
2023-07-04 18:59:00 +0000

    Use upstream Gentoo bootstrap script.
    This uses stable Gentoo

--- install_compatibility_layer_cc.sh
+++ install_compatibility_layer_cc.sh
@@ -3,4 +3,5 @@
 module --force purge
 module load gentoo/2020 apptainer
 
+curl https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-prefix.sh -o bootstrap-prefix-upstream.sh
 ./install_compatibility_layer.sh -r soft.computecanada.ca -v 2023 ${1+"$@"}
