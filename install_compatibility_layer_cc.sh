fff8c7dfa14668d7f8c904146c9e7f756f5910e1
Bart Oldeman <bart.oldeman@calculquebec.ca>
2023-07-14 17:52:28 +0000

    Use specific git commit for bootstrap script.
    Newer commits have some issues with newer portage setting a
    different PATH

--- install_compatibility_layer_cc.sh
+++ install_compatibility_layer_cc.sh
@@ -3,5 +3,6 @@
 module --force purge
 module load gentoo/2020 apptainer
 
-curl https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-prefix.sh -o bootstrap-prefix-upstream.sh
+curl "https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-prefix.sh?id=572c1a5a03ea459d305754ccc25de70dffaaa196" -o bootstrap-prefix-upstream.sh
+
 ./install_compatibility_layer.sh -r soft.computecanada.ca -v 2023 ${1+"$@"}
