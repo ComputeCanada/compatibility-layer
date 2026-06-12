03c3e0b9a92a224e4964f791ce2dfc4646f11818
Bart Oldeman <bart.oldeman@calculquebec.ca>
2026-06-12 20:10:47 +0000

    Update Gentoo and Prefix commit IDs, version number

--- install_compatibility_layer_cc.sh
+++ install_compatibility_layer_cc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 module --force purge
-module load gentoo/2020 apptainer
+module load gentoo/2023 apptainer
 
-curl "https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-prefix.sh?id=572c1a5a03ea459d305754ccc25de70dffaaa196" -o bootstrap-prefix-upstream.sh
+curl "https://gitweb.gentoo.org/repo/proj/prefix.git/plain/scripts/bootstrap-prefix.sh?id=18b2060485f5d66a50c6bd7dcf2dd628cf89f0af" -o bootstrap-prefix-upstream.sh
 sed -i 's/cmd=\"emerge -v -/cmd=\"emerge --ask=n -v -/' bootstrap-prefix-upstream.sh
 sed -i 's/emerge -u /emerge --ask=n -u /' bootstrap-prefix-upstream.sh
 
 unset SSL_CERT_FILE
 unset CURL_CA_BUNDLE
-./install_compatibility_layer.sh -r soft.computecanada.ca -v 2023 ${1+"$@"}
+./install_compatibility_layer.sh -r soft.computecanada.ca -v 2026 ${1+"$@"}
